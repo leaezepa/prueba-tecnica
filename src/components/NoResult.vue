6ab4cc20a9e3d553bb64ff098579062d3ded7771
lean <leandro@eloyaltymarketing.com>
2024-10-28 07:24:08 -0300

    release: commit final

--- src/components/NoResult.vue
+++ src/components/NoResult.vue
@@ -2,7 +2,7 @@
   <div class="font-lato w-full min-h-screen text-center flex flex-col items-center gap-3 py-6">
     <h1 class="text-[36px] font-bold text-gray-800">Uh-oh!</h1>
     <p class="text-xl font-medium text-gray-600">You look lost on your journey!</p>
-    <ButtonAll :bgColor="'bg-rose-600'" class="text-white font-bold mt-4">
+    <ButtonAll :bgColor="'bg-rose-600'" class="text-white font-bold mt-4 hover:bg-red-700">
       <router-link :to="'/'">
         Go back home
       </router-link>
